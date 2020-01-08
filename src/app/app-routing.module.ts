fb5a80f4737fc2e77d0030959a3310f8fa6f535d
harshilokinendi <harshulokinendi@gmail.com>
2020-01-08 14:42:23 +0530

    final project created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ColorComponent } from './color/color.component';
+import { UserComponent } from './user/user.component';
+import { CountryComponent } from './country/country.component';
 
+const routes: Routes = [
+  {path: 'home',component:HomeComponent},
+  {path: 'color',component:ColorComponent},
+  {path: 'country',component:CountryComponent},
+  {path: 'user',component:UserComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
